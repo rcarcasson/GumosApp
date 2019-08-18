55070506b11cba673e2680f46b5e734e464e82d7
rcarcasson <ricardo.carcasson@gmail.com>
2019-08-18 18:57:30 -0400

    Actualización de Endpoint para obtener datos

--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -4,7 +4,7 @@ import { InfoClan, SiguientesCofres } from '../interfaces/interface';
 
 // tslint:disable-next-line: max-line-length
 const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjUxNiwiaWRlbiI6IjQ2OTU0ODQyMjQ0MjEyMzI2NCIsIm1kIjp7fSwidHMiOjE1NTU0MjY1MzY1NzF9.BgfzC6GBMekbWKyosN8gygn_FQu5Be_tumXYOym3N8Q';
-const url = 'https://api.royaleapi.com';
+const url = 'https://api-v2.royaleapi.com';
 const clan = 'QU89QL';
 const headers = new HttpHeaders({
   'Authorization' : `Bearer ${apiKey}`
